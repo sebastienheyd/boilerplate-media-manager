7fdd35013523a151383d4763606953e0f7d118b1
Sebastien HEYD <sebastien.heyd@free.fr>
2021-05-07 12:24:20 +0200

    Load plugins and fonts

--- src/resources/views/index-mce.blade.php
+++ src/resources/views/index-mce.blade.php
@@ -8,10 +8,12 @@
     <meta name="robots" content="noindex, nofollow">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name') }}</title>
+    @stack('css')
+    <link rel="stylesheet" href="{{ mix('/plugins/fontawesome/fontawesome.min.css', '/assets/vendor/boilerplate') }}">
     <link rel="stylesheet" href="{{ mix('/adminlte.min.css', '/assets/vendor/boilerplate') }}">
     <link rel="stylesheet" href="{{ mix('/mediamanager.min.css', '/assets/vendor/boilerplate-media-manager') }}">
-    <style>.content-wrapper { margin-left: 0 }</style>
-    @stack('css')
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet">
 </head>
 <body class="sidebar-mini">
 <div id="disable"></div>
