f5f8cab23d6fa5a2c272ebcc436696856eb452b7
Sebastien HEYD <sebastien.heyd@free.fr>
2021-05-07 12:24:33 +0200

    Assets update

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,23 +1,24 @@
-let mix = require('laravel-mix');
-const Clean = require('clean-webpack-plugin');
+const mix = require('laravel-mix');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-mix.webpackConfig({plugins: [new Clean(['public'], {verbose: false})]})
+mix.webpackConfig({plugins: [new CleanWebpackPlugin()]})
     .setPublicPath("public")
-    .setResourceRoot('/');
+    .setResourceRoot('/assets/vendor/boilerplate-media-manager')
+    .version();
 
-mix.sass('resources/scss/mediamanager.scss', 'public/mediamanager.min.css').version();
-mix.scripts('resources/js/mediamanager.js', 'public/mediamanager.min.js').version();
+mix.sass('resources/scss/mediamanager.scss', 'public/mediamanager.min.css');
+mix.scripts('resources/js/mediamanager.js', 'public/mediamanager.min.js');
 
-mix.sass('resources/scss/select-media.scss', 'public/select-media.min.css').version();
-mix.scripts('resources/js/select-media.js', 'public/select-media.min.js').version();
+mix.sass('resources/scss/select-media.scss', 'public/select-media.min.css');
+mix.scripts('resources/js/select-media.js', 'public/select-media.min.js');
 
 mix.scripts([
     'node_modules/jquery-ui/ui/widget.js',
     'node_modules/blueimp-file-upload/js/jquery.iframe-transport.js',
     'node_modules/blueimp-file-upload/js/jquery.fileupload.js'
-], 'public/vendor/blueimp-file-upload/jquery.fileupload.min.js').version();
+], 'public/vendor/blueimp-file-upload/jquery.fileupload.min.js');
 
 mix.scripts([
     'node_modules/jquery-lazy/jquery.lazy.min.js',
     'node_modules/jquery-lazy/jquery.lazy.plugins.js'
-], 'public/vendor/jquery-lazy/jquery.lazy.plugins.js').version();
+], 'public/vendor/jquery-lazy/jquery.lazy.plugins.js');
